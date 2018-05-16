d4f025b4ec68edbe6fce8f835c20f7d26ed7f394
3414546 <3414546@ppti-14-403-14.ufr-info-p6.jussieu.fr>
2018-05-16 14:06:11 +0200

    CowService

--- Dungeon_Master/src/dungeon/master/mbt/test/CowServiceTest.java
+++ Dungeon_Master/src/dungeon/master/mbt/test/CowServiceTest.java
@@ -1,25 +1,66 @@
 package dungeon.master.mbt.test;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import dungeon.master.components.Cow;
 import dungeon.master.contracts.CowServiceContract;
+import dungeon.master.enumerations.Dir;
+import dungeon.master.exceptions.InvariantError;
+import dungeon.master.exceptions.PreconditionError;
 import dungeon.master.services.CowService;
 
-public class CowServiceTest {
+public class CowServiceTest extends MobServiceTest {
 	private CowService cow;
 	
 	@Before
 	public void beforeTests() {
+		super.beforeTests();
 		cow = new CowServiceContract(new Cow());
+		mob = cow;
 	}
 	
 	
 	@After
 	public void afterTests() {
+		super.afterTests();
 		cow = null;
 	}
 	
+	@Test
+	public void initPreTest_1(){
+		//cas positif
+		try{
+			cow.init(env, 5, 6, Dir.N);
+		}catch(PreconditionError pe){
+			fail();
+		}
+	}
 	
+	@Test
+	public void initPreTest_2(){
+		//cas negatif
+		try{
+			cow.init(env, 55, 6, Dir.N);
+			fail();
+		}catch(PreconditionError pe){
+			
+		}
+	}
+	
+	@Test
+	public void stepTransitionTest_1(){
+		try{
+			cow.init(env, 5, 7, Dir.N);
+			cow.step();
+			assertTrue(cow.getCol() >= 4 && cow.getCol() <= 6 &&
+					cow.getRow() >= 6 && cow.getCol() <= 8);
+		}catch(InvariantError ie){
+			fail();
+		}
+		
+	}
 }
